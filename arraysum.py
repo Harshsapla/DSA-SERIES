a5e3203128d014cfa524008c6eb15d9a3f6863a4
Harsh Yadav <harshsapla03@gmail.com>
2025-01-10 21:12:46 +0530

    plusone

--- arraysum.py
+++ arraysum.py
@@ -72,7 +72,21 @@
 #                return i
                
 #         return len(nums),
-    
+# this is for the plus one problem
+class Solution:
+    def plusone(self,digits) :
+        n=len(digits)
+        for i in range(n-1 , -1 , -1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            else :
+                digits[i]=0
+        return [1]+digits
+solution = Solution()
+digits = [1,2]       
+result = solution.plusone(digits)
+print(result)
                       
 
         
